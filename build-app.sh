1fcac6668ba5d8e636e02927f168524d153448dd
Vid <vid_github@zooid.org>
2014-11-16 19:39:45 -0500

    fixups

--- build-app.sh
+++ build-app.sh
@@ -1,4 +1,4 @@
 ./build-dest.sh
 node nwbuild.js &&
-sed -i 's/udev\.so\.0/udev.so.1/g'  build/ixmaps-node-server/linux64/ixmaps-node-server
+sed -i 's/udev\.so\.0/udev.so.1/g'  build/ixmaps-webkit/linux64/ixmaps-webkit
 
