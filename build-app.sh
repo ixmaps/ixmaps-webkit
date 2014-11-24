eae63190d90c406701bb4106fa49db3db1d9638f
Vid <vid_github@zooid.org>
2014-11-24 16:25:18 -0500

    adapting to new design

--- build-app.sh
+++ build-app.sh
@@ -1,4 +1,9 @@
 ./build-dest.sh
+cd dest
+
+zip -pr ../ixmaps.nw *
+cd ..
+
 node nwbuild.js &&
 # fix for ubuntu libs
 sed -i 's/udev\.so\.0/udev.so.1/g'  build/ixmaps-webkit/linux64/ixmaps-webkit
--- build-dest.sh
+++ build-dest.sh
@@ -2,11 +2,14 @@ rm ixmaps.nw
 rm -rf dest
 mkdir dest
 
-cp -a node_modules/ixmaps-node-server/* dest
-cp -a node_modules/ixmaps-chrome-extension/src/*js dest
-cp -a src/* dest
-cd dest
+SRCDIR=node_modules
 
-zip -pr ../ixmaps.nw *
-cd ..
+if [ -d ../ixmaps-node-server ] && [ -d ../ixmaps-chrome-extension ]; then
+  echo "building from local version"
+  SRCDIR=..
+fi
+
+cp -a $SRCDIR/ixmaps-node-server/* dest
+cp -a $SRCDIR/ixmaps-chrome-extension/src/*js dest
+cp -a src/* dest
 
