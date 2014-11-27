f8fef9a83a1794bb9e40d6a09428748df0ea86ad
Vid <vid_github@zooid.org>
2014-11-27 08:11:28 -0500

    organize code, option to submit to ixmaps

--- src/main.js
+++ src/main.js
@@ -11,10 +11,27 @@ var TRSETS_BASE = 'http://ixmaps.ca/trsets/';
 var processing = {}, completed = {}, freeze = false;
 
 $(document).ready(function() {
+
+// Load initial settings
+  var data = formValues();
+  console.log('default data', data);
+  traceLib.init(data);
+
+  if (traceLib.valid) {
+    traceLib.stored.forEach(function(field) {
+      $('#' + field).val(data[field]);
+    });
+  }
+  resetUI();
+
+  // kick off the queue
+  requests.processQueue();
+
   // add copy/paste for MacOS
   var gui = require('nw.gui');
   var win = gui.Window.get();
   var nativeMenuBar = new gui.Menu({ type: "menubar" });
+
   try {
     nativeMenuBar.createMacBuiltin("My App");
     win.menu = nativeMenuBar;
@@ -22,6 +39,7 @@ $(document).ready(function() {
     console.log(ex.message);
   }
 
+  // # Setup operating environment
   $('#debug').click(function() {
     if (this.checked) {
       require('nw.gui').Window.get().showDevTools();
@@ -29,6 +47,10 @@ $(document).ready(function() {
     } else {
       requests.setDebug(false);
     }
+  });
+
+  $('#doSubmit').click(function() {
+    requests.setSubmit(this.checked);
   });
 
   $('#nparallel').change(function() {
@@ -49,6 +71,46 @@ $(document).ready(function() {
     showDetails($('#viewport'));
   });
 
+  // User has selected from a list
+  $('.selectHost').on('change', function() {
+    if ($(this).val()) {
+      $('#trhost').val($(this).val());
+    }
+  });
+
+  // Bind new settings
+  $('#saveConfig').click(function() {
+    data = formValues();
+    traceLib.init(data);
+    resetUI();
+  });
+
+  // Submit trset or URL
+  $('#traceHost').click(function() {
+    var data = formValues();
+    data.type = 'submitted';
+    data.data = $('#trhost').val();
+    data.tag = $('#tag').val();
+
+    requests.processRequests({requests: [data]}, requests.processIncoming);
+  });
+
+  // Retrieve the ixmaps trsets and add them to the select
+  $.get(TRSETS_BASE, function(data) {
+    $('#trsets').html(data);
+    var links = $('#trsets a');
+    // re-populate trset select
+    $('#trsets a').each(function(k) {
+      var l = $(this).attr('href');
+      if (l.match(/.*\.trset$/)) {
+        l = l.replace('./', '').replace('.trset', '').replace(/_/g, ' ');
+        $('#trset').append($('<option />').text(l));
+      }
+    });
+  });
+
+  // # Operating functions
+
   // provide progress to running traces
   requests.setTraceProgressCB(function(err, update) {
     processing[update.traceID] = update;
@@ -58,11 +120,12 @@ $(document).ready(function() {
   requests.setTraceDoneCB(function(err, update) {
     var traceID = update.traceID;
     console.log('done, removing', traceID);
-    completed[traceID] = processing[traceID];
+    completed[traceID] = $.extend({}, processing[traceID], update);
     delete processing[traceID];
     showDetails($('#viewport'));
   });
 
+  // general summary of traces
   function showDetails($vp) {
     var state = requests.getState();
     $('#processing').html(Object.keys(state.processingHosts).length);
@@ -89,40 +152,20 @@ $(document).ready(function() {
     for (t in toshow) {
       c = (processing[t] ? 'processing' : 'completed');
       table += '<th class="' + c + '">' +  t + '</th>';
-      var update = toshow[t], now = update.now;
+      var update = toshow[t];
       tdata += '<td class="' + c + '">' + (
-         $('#traceView').val() === 'details' ? (now.getMonth() + 1) + '/' + now.getDate() + ' ' + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + "." + now.getMilliseconds() + '\n' + Object.keys(update).map(function(k) { return '<b>' + k + '</b>: ' + JSON.stringify(update[k]); }).join('\n')
+         $('#traceView').val() === 'details' ?  summarizeUpdate(update)
         : '<pre>' + update.buffer + '</pre>'
       ) + '</td>';
     }
     $vp.append(table + '</tr><tr>' + tdata + '</tr></table>');
   }
 
-  // retrieve the ixmaps trsets and add them to the select
-  $.get(TRSETS_BASE, function(data) {
-    $('#trsets').html(data);
-    var links = $('#trsets a');
-    // re-populate trset select
-    $('#trsets a').each(function(k) {
-      var l = $(this).attr('href');
-      if (l.match(/.*\.trset$/)) {
-        l = l.replace('./', '').replace('.trset', '').replace(/_/g, ' ');
-        $('#trset').append($('<option />').text(l));
-      }
-    });
-  });
-
-  // Load initial settings
-  var data = formValues();
-  console.log('default data', data);
-  traceLib.init(data);
-
-  if (traceLib.valid) {
-    traceLib.stored.forEach(function(field) {
-      $('#' + field).val(data[field]);
-    });
+  function summarizeUpdate(update) {
+    var now = update.now;
+    return (now.getMonth() + 1) + '/' + now.getDate() + ' ' + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + "." + now.getMilliseconds() + '\n' +
+      Object.keys(update).map(function(k) { return '<b>' + k + '</b>: ' + JSON.stringify(update[k]); }).join('\n');
   }
-  resetUI();
 
   // show options according to config validity
   function resetUI() {
@@ -132,33 +175,6 @@ $(document).ready(function() {
       $('.needsConfig').hide();
     }
   }
-
-  // User has selected from a list
-  $('.selectHost').on('change', function() {
-    if ($(this).val()) {
-      $('#trhost').val($(this).val());
-    }
-  });
-
-  // Bind new settings
-  $('#saveConfig').click(function() {
-    data = formValues();
-    traceLib.init(data);
-    resetUI();
-  });
-
-  // Submit trset or URL
-  $('#traceHost').click(function() {
-    var data = formValues();
-    data.type = 'submitted';
-    data.data = $('#trhost').val();
-    data.tag = $('#tag').val();
-
-    requests.processRequests({requests: [data]}, requests.processIncoming);
-  });
-
-  // kick off the queue
-  requests.processQueue();
 
 });
 
