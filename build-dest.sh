0f625899b2c387c489ecf3bd548807b8cfa8be5d
Vid <vid_github@zooid.org>
2014-12-16 11:05:40 -0500

    check if general file exists (for symlink)

--- build-dest.sh
+++ build-dest.sh
@@ -4,12 +4,12 @@ mkdir dest
 
 SRCDIR=node_modules
 
-if [ -d ../ixmaps-node-server ] && [ -d ../ixmaps-chrome-extension ]; then
+if [ -e ../ixmaps-node-server ] && [ -e ../ixmaps-chrome-extension ]; then
   echo "building from local version"
   SRCDIR=..
 fi
 
-cp -a $SRCDIR/ixmaps-node-server/* dest
-cp -a $SRCDIR/ixmaps-chrome-extension/src/*js dest
-cp -a src/* dest
+rsync -a $SRCDIR/ixmaps-node-server/* dest
+rsync -a $SRCDIR/ixmaps-chrome-extension/src/*js dest
+rsync -a src/* dest
 
